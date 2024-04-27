ffcc7ce24a3988ae670d1a94c1a7779699ad4a5e
Andrey Arzamaskov <arzamaskov@gmail.com>
2024-04-27 20:24:38 +0400

    create todo contexts

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,19 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+import React from "react";
+import { createRoot } from "react-dom/client";
+import TodoTable from "./components/TodoTable";
+import TodoContextProvider from "./contexts/TodoContext";
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+class App extends React.Component {
+  render() {
+    return (
+      <TodoContextProvider>
+        <TodoTable />
+      </TodoContextProvider>
+    );
+  }
+}
 
-// start the Stimulus application
-import './bootstrap';
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />);
